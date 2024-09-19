19effbab1a7aed2c2a36e37c36efffc859500976
Ocean Evers-Peete <oeverspe@gmail.com>
2024-09-19 20:26:11 +0900

    running, adjusting versions

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@
 
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
+import { structureTool } from 'sanity/structure'
 import {
   defineUrlResolver,
   Iframe,
@@ -39,7 +39,7 @@ export default defineConfig({
   //edit schemas in './src/schemas'
   schema,
   plugins: [
-    deskTool({
+    structureTool({
       // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
       // You can add any React component to `S.view.component` and it will be rendered in the pane
       // and have access to content in the form in real-time.
