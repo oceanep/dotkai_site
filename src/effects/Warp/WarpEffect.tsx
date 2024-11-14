4117ae75886d54178831108b0e6528523ff1941f
oceanep <oeverspe@gmail.com>
2024-11-14 18:10:50 +0900

    moved glsl to independent file, added webpack and ts config for glsl

--- src/effects/Warp/WarpEffect.tsx
+++ src/effects/Warp/WarpEffect.tsx
@@ -1,26 +1,13 @@
 import { Effect, BlendFunction } from "postprocessing";
 import { Uniform, WebGLRenderer, WebGLRenderTarget } from "three";
 import { IWarpProps } from "~/utils/types";
-
-const fragmentShader = /* glsl */`
-    uniform float frequency;
-    uniform float amplitude;
-    uniform float time;
-    
-    void mainUv(inout vec2 uv) {
-        uv.y += sin(uv.x * frequency + time) * amplitude;
-    }
-
-    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-        outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
-    }
-`
+import WarpFragmentShader from "./WarpFragmentShader.glsl";
 
 export default class WarpEffect extends Effect {
     constructor({ frequency, amplitude, blendFunction = BlendFunction.DARKEN }: IWarpProps) {
         super(
             'WarpEffect',
-            fragmentShader,
+            WarpFragmentShader,
             {
                 uniforms: new Map([
                     ['frequency', new Uniform(frequency)],
---
+++ src/effects/Warp/WarpFragmentShader.glsl
@@ -0,0 +1,11 @@
+uniform float frequency;
+uniform float amplitude;
+uniform float time;
+
+void mainUv(inout vec2 uv) {
+    uv.y += sin(uv.x * frequency + time) * amplitude;
+}
+
+void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
+    outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
+}
