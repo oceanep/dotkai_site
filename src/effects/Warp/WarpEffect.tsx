8b6148f520fbf9a0be61b2d7a2cca5fd419131fa
oceanep <oeverspe@gmail.com>
2024-11-15 21:24:18 +0900

    CRT shader working

--- src/effects/Warp/WarpEffect.tsx
+++ src/effects/Warp/WarpEffect.tsx
@@ -1,24 +1,25 @@
 import { Effect, BlendFunction } from "postprocessing";
-import { Uniform, WebGLRenderer, WebGLRenderTarget } from "three";
+import { Uniform, Vector2, WebGLRenderer, WebGLRenderTarget } from "three";
 import { IWarpProps } from "~/utils/types";
 import WarpFragmentShader from "./WarpFragmentShader.glsl";
 
 export default class WarpEffect extends Effect {
     constructor({ frequency, amplitude, blendFunction = BlendFunction.DARKEN }: IWarpProps) {
+        const resolution = new Vector2(window.innerWidth, window.innerHeight)
         super(
             'WarpEffect',
             WarpFragmentShader,
             {
                 uniforms: new Map([
-                    ['frequency', new Uniform(frequency)],
-                    ['amplitude', new Uniform(amplitude)],
-                    ['time', new Uniform(0)]
+                    ['warp', new Uniform(0.75)],
+                    ['scan', new Uniform(0.75)],
+                    ['iResolution', new Uniform(resolution) as Uniform<any>]
                 ]),
                 blendFunction
             }
         )
     }
-    update(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget, deltaTime?: number): void {
-        this.uniforms.get('time').value += deltaTime
-    }
+    // update(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget, deltaTime?: number): void {
+    //     this.uniforms.get('time').value += deltaTime
+    // }
 }
--- src/effects/Warp/WarpFragmentShader.glsl
+++ src/effects/Warp/WarpFragmentShader.glsl
@@ -1,11 +1,30 @@
-uniform float frequency;
-uniform float amplitude;
-uniform float time;
 
-void mainUv(inout vec2 uv) {
-    uv.y += sin(uv.x * frequency + time) * amplitude;
-}
 
-void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-    outputColor = vec4(0.8, 1.0, 0.5, inputColor.a);
-}
+  uniform vec2 iResolution;
+  uniform float warp;
+  uniform float scan;
+  uniform sampler2D tDiffuse; // Input buffer from the rendered scene
+
+  void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 fragColor) {
+    vec2 coord = uv * iResolution.xy;
+    vec2 dc = abs(0.5 - uv);
+    dc *= dc;
+
+    // Apply warp effect to UV coordinates
+    vec2 warpedUv = uv;
+    warpedUv.x -= 0.5; 
+    warpedUv.x *= 1.0 + (dc.y * (0.3 * warp)); 
+    warpedUv.x += 0.5;
+    warpedUv.y -= 0.5; 
+    warpedUv.y *= 1.0 + (dc.x * (0.4 * warp)); 
+    warpedUv.y += 0.5;
+
+    // Sample the input buffer and apply scanline effect
+    if (warpedUv.y > 1.0 || warpedUv.x < 0.0 || warpedUv.x > 1.0 || warpedUv.y < 0.0) {
+      fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+      float apply = abs(sin(coord.y * 0.05) * 0.5 * scan);
+      vec3 color = texture2D(tDiffuse, warpedUv).rgb;
+      fragColor = vec4(mix(color, vec3(0.0), apply), 1.0);
+    }
+  }
