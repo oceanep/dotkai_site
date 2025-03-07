f6f447ef6b384b9db712aadff93186273d23df5d
oceanep <oeverspe@gmail.com>
2025-03-07 15:59:37 +0700

    static representation built

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useLiveQuery } from 'next-sanity/preview'
-import { Bvh, Plane } from '@react-three/drei'
+import { Bvh, Html, Plane } from '@react-three/drei'
 
 import { readToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
@@ -37,25 +37,46 @@ const R3F = ({projects}) => {
     console.log('projects: ', projects.projects)
     
     return (
-        <Bvh>
-            {projects.projects.length > 0 ? (
-                projects.projects.map((project, i) => 
-                    <Plane 
-                      args={[.25, .25]}
-                      rotation={[0, Math.PI / 6, 0]}
+        // <Bvh>
+            <>
+              <group
+                position={[-1.5, 1.5, 0]}
+                rotation={[0, Math.PI / 4, 0]}
+              >
+                {projects.projects.length > 0 ? (
+                  projects.projects.map((project, i) => 
+                    <mesh
+                      rotation={[0, 0, 0]}
                       position={[
                         i === 0 ? i : i / 3,
-                        -0.5,
-                        i === 0 ? i : -i / 5
+                        0,
+                        0
                       ]}
                       key={`${project.slug}-${i}`}
                     >
-                      <meshBasicMaterial attach="material" color="blue" />
-                    </Plane>)
-            ) : (
-              <ComputerMesh scale={0.2} />
-            )}
-        </Bvh>
+                      <Plane 
+                        args={[.25, .25]}
+                      >
+                        <meshBasicMaterial attach="material" color="blue" />
+                      </Plane>    
+                    </mesh>)
+                ) : (
+                  <ComputerMesh scale={0.2} />
+                )}
+              </group>
+              <group position={[0.9, .5, 0]}>
+                <Plane args={[2, 2.5]}>
+                  <meshBasicMaterial attach="material" color="blue" />
+                  {/* <Html position={[0, 0, 0]} transform>
+                    <div style={{ margin: '5%', backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>
+                      <h1>HTML Content</h1>
+                      <p>This is some HTML content inside a 3D plane.</p>
+                    </div>
+                  </Html> */}
+                </Plane>
+              </group>
+            </>
+        // </Bvh> 
     );
 };
 
